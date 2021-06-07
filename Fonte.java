4b9c39240121c07c4b7dd6954fb8feb1365fcf0f
Pedro Vilaça <pedrovilacav@gmail.com>
2021-06-07 19:17:30 -0300

    Adicionada forma de inserir dados de teste e medição de tempo nas funções de buscar, excluir e editar

--- Fonte.java
+++ Fonte.java
@@ -8,6 +8,7 @@
 // import java.io.RandomAccessFile;
 // import java.util.ArrayList;
 // import java.lang.reflect.Constructor;
+// import java.util.Random;
 
 // public interface RegistroProntuario<T> {
 
@@ -21,7 +22,7 @@
 
 // }
 
-// class Main {
+// public class Main {
 //   public static void main(String[] args) {
 
 //     HashExtensivel<Prontuario> hash;
@@ -46,6 +47,7 @@
 //         System.out.println("3 - Excluir prontuário");
 //         System.out.println("4 - Editar Diagnóstico");
 //         System.out.println("5 - Imprimir prontuários disponíveis");
+//         System.out.println("6 - Inserir dados de teste");
 //         System.out.println("0 - Sair do sistema");
 
 //         try {
@@ -78,7 +80,12 @@
 //             System.out.print("CPF: ");
 //             int cpf = console.nextInt();
 //             startTime = System.nanoTime();
-//             System.out.println("Dados: " + hash.read(String.valueOf(cpf).hashCode()));
+//             Prontuario temp = hash.read(String.valueOf(cpf).hashCode());
+//             if (temp == null) {
+//               System.out.println("\nCPF não encontrado na base de dados");
+//               break;
+//             }
+//             System.out.println("Dados: " + temp);
 //             endTime = System.nanoTime();
 //             System.out.println("Tempo de busca em nanosegundos: " + (endTime - startTime));
 //           }
@@ -104,8 +111,14 @@
 //             Prontuario temp = hash.read(String.valueOf(cpf).hashCode());
 //             endTime = System.nanoTime();
 //             System.out.println("Tempo de busca em nanosegundos: " + (endTime - startTime));
+//             if (temp == null) {
+//               System.out.println("\nCPF não encontrado na base de dados");
+//               break;
+//             }
 //             System.out.println("\nDigite a alteração do diagnóstico:");
-//             String diag = console.next();
+//             console.nextLine();
+//             String diag;
+//             diag = console.nextLine();
 //             temp.setDiagnostico(diag);
 //             hash.update(temp);
 //             System.out.println("\nDiagnóstico alterado com sucesso");
@@ -114,6 +127,35 @@
 //           case 5: {
 //             System.out.println("\nIMPRIMINDO PRONTUÁRIOS:");
 //             hash.print();
+//           }
+//             break;
+//           case 6: {
+//             // opção para inserir dados aleatorios
+//             System.out.println("Inserindo dados de teste: ");
+//             Random gerador = new Random();
+//             int qtdInicial = 100000000;
+//             int qtdMax = 100090000;
+//             // int qtdMax = 100030000;
+//             // int qtdMax = 999999999;
+//             int cpf;
+//             String nome = "";
+//             String data_nasc = "";
+//             String sexo = "sexo teste";
+//             String diagnostico = "";
+
+//             for (cpf = qtdInicial; cpf < qtdMax; cpf++) {
+//               nome = gerarNomeAleatorio(gerador);
+//               data_nasc = gerarDataAleatoria(gerador);
+//               sexo = gerarSexoAleatorio(gerador);
+//               diagnostico = gerarDiagnosticoAleatorio(gerador);
+//               // System.out.println("Nome: " + nome);
+//               System.out.println("CPF: " + cpf);
+//               // System.out.println("Data Nascimento: " + data_nasc);
+//               // System.out.println("Sexo: " + sexo);
+//               // System.out.println("Diagnostico: " + diagnostico + "\n");
+//               hash.create(new Prontuario(nome, data_nasc, sexo, diagnostico, cpf));
+//               // hash.print();
+//             }
 //           }
 //             break;
 //           case 0:
@@ -127,6 +169,38 @@
 //       e.printStackTrace();
 //     }
 //     console.close();
+//   }
+
+//   public static String gerarSexoAleatorio(Random gerador) {
+//     if (gerador.nextInt(2) == 0) {
+//       return "Feminino";
+//     }
+//     return "Masculino";
+//   }
+
+//   public static String gerarDataAleatoria(Random gerador) {
+//     return (gerador.nextInt(31)) + 1 + "-" + (gerador.nextInt(12) + 1) + "-" + "1984";
+//   }
+
+//   public static String gerarDiagnosticoAleatorio(Random gerador) {
+//     String diagnostico = "Diagnostico_teste ";
+//     String palavra;
+//     for (int i = 0; i < 4; i++) {
+//       palavra = "";
+//       for (int y = 0; y < 5; y++) {
+//         palavra += ((char) (gerador.nextInt(25) + 65));
+//       }
+//       diagnostico += (palavra + " ");
+//     }
+//     return diagnostico;
+//   }
+
+//   public static String gerarNomeAleatorio(Random gerador) {
+//     String diagnostico = "Nome_teste ";
+//     for (int y = 0; y < 10; y++) {
+//       diagnostico += ((char) (gerador.nextInt(25) + 65));
+//     }
+//     return diagnostico;
 //   }
 // }
 
--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 import Hashing.HashExtensivel;
 import java.io.File;
+import java.util.Random;
 
 public class Main {
   public static void main(String[] args) {
 
-    HashExtensivel<Prontuario> hash;  
+    HashExtensivel<Prontuario> hash;
     Scanner console = new Scanner(System.in);
     long startTime, endTime;
 
@@ -27,6 +28,7 @@ public class Main {
         System.out.println("3 - Excluir prontuário");
         System.out.println("4 - Editar Diagnóstico");
         System.out.println("5 - Imprimir prontuários disponíveis");
+        System.out.println("6 - Inserir dados de teste");
         System.out.println("0 - Sair do sistema");
 
         try {
@@ -59,11 +61,16 @@ public class Main {
             System.out.print("CPF: ");
             int cpf = console.nextInt();
             startTime = System.nanoTime();
-            System.out.println("Dados: " + hash.read(String.valueOf(cpf).hashCode()));
+            Prontuario temp = hash.read(String.valueOf(cpf).hashCode());
+            if (temp == null) {
+              System.out.println("\nCPF não encontrado na base de dados");
+              break;
+            }
+            System.out.println("Dados: " + temp);
             endTime = System.nanoTime();
             System.out.println("Tempo de busca em nanosegundos: " + (endTime - startTime));
           }
-          break;
+            break;
           case 3: {
             System.out.println("\nEXCLUSÃO DE PRONTUÁRIO");
             System.out.println("\nDigite o CPF para prosseguir.");
@@ -75,7 +82,7 @@ public class Main {
             System.out.println("Tempo de busca e exclusão em nanosegundos: " + (endTime - startTime));
             hash.print();
           }
-          break;
+            break;
           case 4: {
             System.out.println("\nBUSQUE O CPF PARA EDITAR O DIAGNÓSTICO");
             System.out.println("\nDigite o CPF para prosseguir.");
@@ -85,16 +92,51 @@ public class Main {
             Prontuario temp = hash.read(String.valueOf(cpf).hashCode());
             endTime = System.nanoTime();
             System.out.println("Tempo de busca em nanosegundos: " + (endTime - startTime));
+            if (temp == null) {
+              System.out.println("\nCPF não encontrado na base de dados");
+              break;
+            }
             System.out.println("\nDigite a alteração do diagnóstico:");
-            String diag = console.nextLine();
+            console.nextLine();
+            String diag;
+            diag = console.nextLine();
             temp.setDiagnostico(diag);
             hash.update(temp);
             System.out.println("\nDiagnóstico alterado com sucesso");
           }
-          break;
+            break;
           case 5: {
             System.out.println("\nIMPRIMINDO PRONTUÁRIOS:");
             hash.print();
+          }
+            break;
+          case 6: {
+            // opção para inserir dados aleatorios
+            System.out.println("Inserindo dados de teste: ");
+            Random gerador = new Random();
+            int qtdInicial = 100000000;
+            int qtdMax = 100090000;
+            // int qtdMax = 100030000;
+            // int qtdMax = 999999999;
+            int cpf;
+            String nome = "";
+            String data_nasc = "";
+            String sexo = "sexo teste";
+            String diagnostico = "";
+
+            for (cpf = qtdInicial; cpf < qtdMax; cpf++) {
+              nome = gerarNomeAleatorio(gerador);
+              data_nasc = gerarDataAleatoria(gerador);
+              sexo = gerarSexoAleatorio(gerador);
+              diagnostico = gerarDiagnosticoAleatorio(gerador);
+              // System.out.println("Nome: " + nome);
+              System.out.println("CPF: " + cpf);
+              // System.out.println("Data Nascimento: " + data_nasc);
+              // System.out.println("Sexo: " + sexo);
+              // System.out.println("Diagnostico: " + diagnostico + "\n");
+              hash.create(new Prontuario(nome, data_nasc, sexo, diagnostico, cpf));
+              // hash.print();
+            }
           }
             break;
           case 0:
@@ -108,5 +150,37 @@ public class Main {
       e.printStackTrace();
     }
     console.close();
+  }
+
+  public static String gerarSexoAleatorio(Random gerador) {
+    if (gerador.nextInt(2) == 0) {
+      return "Feminino";
+    }
+    return "Masculino";
+  }
+
+  public static String gerarDataAleatoria(Random gerador) {
+    return (gerador.nextInt(31)) + 1 + "-" + (gerador.nextInt(12) + 1) + "-" + "1984";
+  }
+
+  public static String gerarDiagnosticoAleatorio(Random gerador) {
+    String diagnostico = "Diagnostico_teste ";
+    String palavra;
+    for (int i = 0; i < 4; i++) {
+      palavra = "";
+      for (int y = 0; y < 5; y++) {
+        palavra += ((char) (gerador.nextInt(25) + 65));
+      }
+      diagnostico += (palavra + " ");
+    }
+    return diagnostico;
+  }
+
+  public static String gerarNomeAleatorio(Random gerador) {
+    String diagnostico = "Nome_teste ";
+    for (int y = 0; y < 10; y++) {
+      diagnostico += ((char) (gerador.nextInt(25) + 65));
+    }
+    return diagnostico;
   }
 }
